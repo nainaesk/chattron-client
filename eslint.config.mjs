4065aacf8101720d8cebc463af06c7794e045627
Naina Sebastian <nainasebastian@Nainas-MacBook-Air.local>
2025-04-21 17:31:25 +0530

    Chore: Updated lint config settings to change behaviour of adding space when saved

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,8 @@ export default tseslint.config(
       ...eslintPluginReactHooks.configs.recommended.rules,
       ...eslintPluginReactRefresh.configs.vite.rules,
       '@typescript-eslint/explicit-function-return-type': 'off',
-      semi: ['error', 'never']
+      semi: ['error', 'never'],
+      'prettier/prettier': ['error']
     }
   },
   eslintConfigPrettier
