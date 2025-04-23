778eca6ab6383a88fe66281eb8d4fc5ecf52de2b
Naina Sebastian <nainasebastian@Nainas-MacBook-Air.local>
2025-04-23 14:53:56 +0530

    Chore: Added mock model to improve code readability

---
+++ src/shared/mock/ChatMessages.ts
@@ -0,0 +1,82 @@
+import { ConversationItem } from '@shared/models'
+
+export const ChatMessagesMock: ConversationItem[] = [
+  {
+    id: '1',
+    message: 'Hey, how are you?',
+    messageTime: '10:30AM',
+    senderName: 'Alice',
+    isSenderUser: false
+  },
+  {
+    id: '2',
+    message: 'I’m good, thanks! How about you? I have been thinking about food a lately.',
+    messageTime: '10:32 AM',
+    senderName: 'You',
+    isSenderUser: true
+  },
+  {
+    id: '3',
+    message: 'I’m doing great! Are we still on for today?',
+    messageTime: '10:35 AM',
+    senderName: 'Alice',
+    isSenderUser: false
+  },
+  {
+    id: '4',
+    message: 'Yes, absolutely! Looking forward to it.',
+    messageTime: '10:40 AM',
+    senderName: 'You',
+    isSenderUser: true
+  },
+  {
+    id: '5',
+    message:
+      'Technology has revolutionized the way we communicate, work, and live, enabling faster connections, greater efficiency, and unprecedented access to information across the globe every day.',
+    messageTime: '10:40 AM',
+    senderName: 'You',
+    isSenderUser: true
+  },
+  {
+    id: '6',
+    message: 'That’s so true! Technology has made life so much easier.',
+    messageTime: '10:45 AM',
+    senderName: 'Alice',
+    isSenderUser: false
+  },
+  {
+    id: '7',
+    message: 'Absolutely! By the way, did you finish the project we discussed?',
+    messageTime: '10:50 AM',
+    senderName: 'You',
+    isSenderUser: true
+  },
+  {
+    id: '8',
+    message: 'Yes, I did! I’ll send it to you shortly.',
+    messageTime: '10:55 AM',
+    senderName: 'Alice',
+    isSenderUser: false
+  },
+  {
+    id: '9',
+    message: 'Great! Let me know if you need any feedback.',
+    messageTime: '11:00 AM',
+    senderName: 'You',
+    isSenderUser: true
+  },
+  {
+    id: '10',
+    message: 'Sure thing! Thanks for your help.',
+    messageTime: '11:05 AM',
+    senderName: 'Alice',
+    isSenderUser: false
+  },
+  {
+    id: '11',
+    message: 'Hi',
+    messageTime: '11:05 AM',
+    senderName: 'Alice',
+    isSenderUser: false
+  }
+]
--- src/shared/models.ts
+++ src/shared/models.ts
@@ -1,1 +1,9 @@
 export type ChatListItemStatus = 'recieved' | 'sent' | 'delivered' | 'seen'
+
+export type ConversationItem = {
+  id: string
+  message: string
+  messageTime: string
+  senderName: string
+  isSenderUser: boolean
+}
